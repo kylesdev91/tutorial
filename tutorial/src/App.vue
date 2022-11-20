79506671e5c3982941d78672162ed0765b185d7e
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-11-20 09:37:12 -0600

    got the form input fields

--- tutorial/src/App.vue
+++ tutorial/src/App.vue
@@ -1,8 +1,39 @@
 <script setup>
+import { useUserStore } from './stores/users';
+import { ref } from 'vue';
+
+const user_store = useUserStore();
+
+const user_input = ref({
+  name: "",
+  email: "",
+})
+
+const sort = ref(false)
+
+const CreateUser = () => {
+  if(!user_input.value.name || !user_input.value.email) {
+    return alert("Please enter both name and email");
+  }
+  user_store.createUser(user_input.value);
+
+  user_input.value = {
+    name: "",
+    email: "",
+  }
+}
 </script>
 
 <template>
-  Hello, World!
+  <main>
+    <h1>Team manager</h1>
+
+    <form @submit.prevent="CreateUser">
+       <input type="text" placeholder="e.g Fat Wewo" v-model="user_input.name" />
+       <input type="email" placeholder="e.g fat@fatgeese.com" v-model="user_input.email" />
+       <input type="submit" value="Create user">
+    </form>
+  </main>
 </template>
 
 <style>
